5b4960f7e626de085908f6dd6a92f2299c88e178
ZhruinVladislav <vladzhurin36@gmail.com>
2022-05-24 23:47:42 +0300

    change 24/05

--- js/index.js
+++ js/index.js
@@ -154,15 +154,13 @@ window.addEventListener('DOMContentLoaded', function() {
       },
 
       768: {
-        slidesPerGroup: 2,
         slidesPerView: 2,
         spaceBetween: 37,
       },
 
       1000: {
         slidesPerGroup: 3,
-        slidesPerView: 2,
-        spaceBetween: 34,
+        slidesPerView: 3,
       },
 
       1415: {
@@ -176,8 +174,9 @@ window.addEventListener('DOMContentLoaded', function() {
     keyboard: {
       enabled: true,
       onlyInViewport: true
-    },
+    }, // можно управлять с клавиатуры стрелками влево/вправо
 
+    // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
     watchSlidesProgress: true,
     watchSlidesVisibility: true,
     slideVisibleClass: "slide-visible",
